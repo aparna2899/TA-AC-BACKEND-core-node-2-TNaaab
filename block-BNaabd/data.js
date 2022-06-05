19f15ca4cf75debb1cff706240658418989a9842
Aparna <tiwari.aparna2899@gmail.com>
2022-06-05 22:41:32 +0530

    Capturing data from request body

--- block-BNaabd/data.js
+++ block-BNaabd/data.js
@@ -4,16 +4,17 @@ var qs = require('querystring');
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
-  var dataFormat = req.headers['content-type'];
   var store = '';
   req.on('data', (chunk) => {
     store = store + chunk;
   });
   req.on('end', () => {
-    if (dataFormat == 'application / json') {
+    if (req.method == 'POST' && req.url == '/json') {
+      res.setHeader('Content-Type', 'application/json');
       res.end(store);
     }
-    if (dataFormat == 'application / x - www - form - urlencoded') {
+    if (req.method == 'POST' && req.url == '/form') {
+      res.setHeader('Content-Type', 'application/x-www-form-urlencoded');
       var parsedData = qs.parse(store);
       res.end(JSON.stringify(parsedData));
     }
